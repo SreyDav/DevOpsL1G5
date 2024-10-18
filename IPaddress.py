9c7ea3e75d48f9ba7e417b5dd262cca282228db9
SreyDav <sithhouy@gmail.com>
2024-10-18 12:17:09 +0700

    Update IPaddress.py

--- IPaddress.py
+++ IPaddress.py
@@ -4,7 +4,7 @@ import json
 
 while True:
     ip = input("What is your target IP: ")
-    url = "http://ip-api.com/json/"
+    url = "https://ip-api.com/json/"
     response = urllib2.urlopen(url + ip)
     data = response.read()
     values = json.loads(data)
